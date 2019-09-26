dd4545ca30206ce36a0c5991d61b23d378c3795b
Maciej Lewicki - Work <lewickima@drexel.edu>
2019-09-26 15:33:22 -0400

    now supports creating playlists

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ import { map, filter, debounceTime, flatMap } from 'rxjs/operators'
 import * as $ from 'jquery'
 
 const authEndpoint = 'https://accounts.spotify.com/authorize'
-const redirectUri = "http://localhost:3000"
+const redirectUri = 'http://localhost:3000'
+const profile = 'https://api.spotify.com/v1/me'
+
+const scopes = [
+      'user-read-private',
+      'user-read-email',
+      'playlist-modify-public'
+];
 
 const hash = window.location.hash.substring(1)
                                  .split('&')
@@ -27,6 +34,10 @@ let spotifyRequestStream = new Subject();
 let spotifyAuthStream = new Subject();
 let spotifyTrackStream = new Subject();
 let spotifyTrackReceiveStream = new Subject();
+let spotifySongStream = new Subject();
+let playlistTrackStream = new Subject();
+let userInfoStream = new Subject();
+let playlistInfoStream = new Subject();
 
 class SearchBar extends React.Component {
     render() {
@@ -50,17 +61,22 @@ class Song extends React.Component {
         super(props);
 
         this.state = {
-            uri: props.info.uri,
             album: props.info.album,
             title: props.info.title,
             artist: props.info.artist,
-            artwork: props.info.artwork
+            artwork: props.info.artwork,
+            id: props.info.id
         };
+
+        if(this.props.onClick) {
+            this.processSong = this.props.onClick;
+            this.processSong = this.processSong.bind(this);
+        }
     }
 
     render() {
         return (
-            <div className="song">
+            <div className="song" onClick={this.processSong}>
                 <img src={this.state.artwork} alt={this.state.album} title={this.state.album}/>
                 <div className="songInfo">
                     <p title={this.state.title}>{this.shorten(this.state.title, 30)}</p>
@@ -85,39 +101,91 @@ class SearchResults extends React.Component {
         super(props);
 
         this.state = {
-            items: [],
+            search: [],
+            items: []
         }
+
+        this.submitSongs = this.submitSongs.bind(this);
     }
 
     componentDidMount() {
         this.initializeTrackStream();
+        this.initializeSongStream();
 
         spotifyRequestStream.subscribe(() => {
             console.log("removing search results");
             this.setState({
-                items: []
+                search: []
             });
         })
     }
 
     render() {
         return (
-            <div className="results">
-                {this.state.items.map(item =>
-                    <Song key={item.uri} info={item} />
-                )}
+            <div className="searchPage">
+                <div className="results">
+                    {this.state.search.map(item =>
+                        <Song key={item.id} onClick={this.processSearchSong} info={item} />
+                    )}
+                </div>
+                <div className="results">
+                    {this.state.items.map(item =>
+                        <Song key={item.id} onClick={this.processResultSong} info={item} />
+                    )}
+                </div>
+                {this.displayButton()}
             </div>
         )
     }
 
     initializeTrackStream() {
         spotifyTrackReceiveStream.pipe(
-            filter(val => !this.state.items.find(id => val.id === id))
+            filter(val => !this.state.search.find(id => val.id === id))
         ).subscribe(val => {
             this.setState({
-                items: this.state.items.concat(val),
+                search: this.state.search.concat(val),
             });
         });
+    }
+
+    initializeSongStream() {
+        spotifySongStream.subscribe(val => {
+            let search = this.state.search.slice();
+            let items = this.state.items.slice();
+
+            if(!val.arg) {
+                this.setState({
+                    items: items.filter(item => item.id !== val.item.id)
+                })
+
+                return null;
+            }
+
+            this.setState({
+                search: search.filter(item => item.id !== val.item.id),
+                items: items.concat(val.item)
+            });
+        });
+    }
+
+    processSearchSong() {
+        spotifySongStream.next({arg: true, item: this.state});
+    }
+
+    processResultSong() {
+        spotifySongStream.next({arg: false, item: this.state});
+    }
+
+    displayButton() {
+        if(this.state.items.length > 0 || this.state.search.length > 0) {
+            return (
+                <button onClick={this.submitSongs}>Create Playlist</button>
+            );
+        } else return null;
+    }
+
+    submitSongs() {
+        playlistTrackStream.next(this.state.items);
     }
 }
 
@@ -139,7 +207,7 @@ class AuthButton extends React.Component {
                     <label htmlFor="search">Client id:</label>
                     <input id="search" type="text" onChange={this.processChange}></input>
                 </div>
-                <a href={`${authEndpoint}?client_id=${this.state.clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`}>
+                <a href={`${authEndpoint}?client_id=${this.state.clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join('%20')}&response_type=token&show_dialog=true`}>
                     Login to Spotify
                 </a>
             </div>
@@ -166,6 +234,9 @@ class SearchApp extends React.Component {
         this.initializeAuthStream();
         this.initializeSearchStream();
         this.initializeTrackStream();
+        this.initializePlaylistStream();
+        this.initializeUserStream();
+        this.initializeInfoStream();
 
         if(hash.access_token) {
             spotifyAuthStream.next(hash.access_token);
@@ -240,6 +311,56 @@ class SearchApp extends React.Component {
             })
         ).subscribe(val => {
             spotifyTrackReceiveStream.next(val);
+        });
+    }
+
+    initializePlaylistStream() {
+        playlistTrackStream.pipe(
+            map(val => val.map(it => 'spotify:track:' + it.id)),
+            map(val => val.join(','))
+        ).subscribe(val => {
+            $.ajax({
+                url: profile,
+                type: "GET",
+                beforeSend: (xhr) => {
+                    xhr.setRequestHeader("Authorization", "Bearer " + this.state.access);
+                },
+                success: (data) => {
+                    userInfoStream.next({tracks: val, id: data.id});
+                }
+            });
+        });
+    }
+
+    initializeUserStream() {
+        userInfoStream.subscribe(val => {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/users/' + val.id + '/playlists',
+                type: 'POST',
+                data: '{"name":"Test playlist", "description": "no u"}',
+                beforeSend: (xhr) => {
+                    xhr.setRequestHeader('Authorization', 'Bearer ' + this.state.access);
+                },
+                success: (data) => {
+                    playlistInfoStream.next({tracks: val.tracks, id: data.id});
+                }
+            })
+        });
+    }
+
+    initializeInfoStream() {
+        playlistInfoStream.subscribe(val => {
+            console.log(val.tracks);
+            $.ajax({
+                url: 'https://api.spotify.com/v1/playlists/' + val.id + '/tracks?uris=' + val.tracks,
+                type: 'POST',
+                beforeSend: (xhr) => {
+                    xhr.setRequestHeader('Authorization', 'Bearer ' + this.state.access);
+                },
+                success: (data) => {
+                    console.log('successfully created playlist');
+                }
+            });
         });
     }
 }
